521e6dc0b9308fb545fb30347a1dd16653253d5c
AndyBory <andyborysov@gmail.com>
2023-02-22 16:27:52 +0200

    First

---
+++ assets/recursionMiniTask.js
@@ -0,0 +1,16 @@
+//Создать рекурсивную функцию вычисления факториала числа. Факториал - произведение всех натуральных чисел от 1 до x.
+// 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+function toFactorial(number) {
+  // debugger;
+  if (typeof(number) !== 'number') {
+    return 'Введите целое число от 1 до 170, свыше 170 результат ∞';
+  }
+  if (number === 0) {
+    return 'Введите целое число от 1 до 170';
+  } else if (number === 1) {
+    return 1;
+  } else if (number > 1) {
+    return number * toFactorial (number - 1);
+  }
+}
