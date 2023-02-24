2db7784f49753e4fbe89a9f38f31419039fec44e
AndyBory <andyborysov@gmail.com>
2023-02-24 15:03:42 +0200

    Update, add errors

--- assets/recursionMiniTask.js
+++ assets/recursionMiniTask.js
@@ -1,17 +1,17 @@
 //Создать рекурсивную функцию вычисления факториала числа. Факториал - произведение всех натуральных чисел от 1 до x.
 // 5! = 1 * 2 * 3 * 4 * 5 = 120
 
-function toFactorial(number) {
+function factorial(number) {
   // debugger;
-  if (typeof(number) !== 'number' || isNaN(number)) {
-    return 'Введите целое число от 1 до 170, свыше 170 результат ∞';
+  if (typeof number !== 'number' || isNaN(number)) {
+    throw new TypeError('Введите целое число от 1 до 170, свыше 170 результат ∞');
   }
-  if (number === 0 || number < 0 || number > 10442) {
-    return 'Введите целое число от 1 до 170';
+  if (number === 0 || number < 0 || number > 10442 || !Number.isInteger(number)) {
+    throw new RangeError('Введите целое число от 1 до 170');
   } 
-  else if (number === 1) {
-    return 1;}
-  else (number > 1) 
-    return number * toFactorial (number - 1);
+  if (number > 1) {
+  return number * factorial (number - 1);
+}
+return 1;
 }
 
